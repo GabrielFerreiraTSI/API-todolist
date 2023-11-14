cc1b8153ebe108705a163c8a919a89364c513087
GabrielFerreiraTSI <117236024+GabrielFerreiraTSI@users.noreply.github.com>
2023-11-14 15:28:19 -0300

    Update date.js
    File date.js was updated.

--- backend/src/date.js
+++ backend/src/date.js
@@ -1,1 +1,14 @@
-module.exports.getData = getData()
+module.exports.getDate = getDate;
+
+function getDate() {
+    const today = new Date();
+
+    let options = {
+        weekday:"longday",
+        month:"numerical",
+        day:"numerical",
+        year:"numerical"
+    };
+
+    return today.toLocaleDateString("pt-BR", options);
+}
