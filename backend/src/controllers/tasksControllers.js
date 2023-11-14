963cadd9bc3ee7d841be9ce5ef5c09cee59caa82
GabrielFerreiraTSI <117236024+GabrielFerreiraTSI@users.noreply.github.com>
2023-11-14 14:47:17 -0300

    UPDATE backend and .gitignore
    Controllers have been changed/updated and the file date.js was created.

--- backend/src/controllers/tasksControllers.js
+++ backend/src/controllers/tasksControllers.js
@@ -1,12 +1,17 @@
 const tasksModel = require("../modules/tasksModel");
 
-
-
 const getAll = async (request, response) => {
     const [tasks] = await tasksModel.getAll();
     return response.status(200).json(tasks);
 };
 
+const createTasks = async (task) => {
+    const {title} = task;
+    const query = "INSERT INTO tasks(id, title, status, created_at) VALUES(?, ?, ?)";
+    const createdTask = await connection.execute(query, [title, "pendente", "data"]);
+}
+
 module.exports = {
-    getAll
+    getAll,
+    createTasks
 }
